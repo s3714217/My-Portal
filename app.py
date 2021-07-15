d7cc56b2b3e8fd647024ff0a8cfe8937365bec2a
Thien Nguyen <41944095+s3714217@users.noreply.github.com>
2021-07-15 22:55:11 +1000

    Add files via upload

--- app.py
+++ app.py
@@ -1,17 +1,15 @@
 from flask import *
 from flask_restful import Api
-from resources.db import Database
-from resources.todo import Todo
+from resources.db import *
 from resources.translate import translate_util
 from datetime import datetime
 from resources.translate import Translate
 import requests
 from flask import request
-
+from flask import copy_current_request_context
 app = Flask(__name__)
 api = Api(app)
 
-api.add_resource(Todo, "/todo/<string:text>/")
 api.add_resource(Translate, "/api/translate/word=<string:word>&language=<string:language>")
 
 @app.route("/")
@@ -24,13 +22,17 @@ def translate_api():
   code_list = translate_util().get_code_list()
   return render_template("translate.html", code_list= code_list)
 
+@app.route("/api/encrypt")
+def encrypt_api():
+  return render_template("encrypt.html")
+
 @app.route("/api")
 def api_menu():
   return render_template("api_menu.html")
 
 @app.route("/guestbook")
 def guestbook():
-  message = Database().get_guestbook_message()
+  message = GuestBookDatabase().get_guestbook_message()
   if len(message) < 1:
     return render_template("guestbook.html", empty=True)
   else:
@@ -46,8 +48,8 @@ def parse_guestbook_request():
       else:
         new_message.append(request.form['name']) 
       new_message.append(request.form['message'])
-    Database().new_guestbook_message(new_message)
-    return render_template("guestbook.html", empty=False,message=Database().get_guestbook_message())
+    GuestBookDatabase().new_guestbook_message(new_message)
+    return render_template("guestbook.html", empty=False,message=GuestBookDatabase().get_guestbook_message())
 
 @app.route("/api/translate", methods=['POST'])
 def parse_translate_request():
@@ -60,6 +62,11 @@ def parse_translate_request():
     else:
       return f"Invalid word."
 
+@app.route('/api/authentication', methods=['POST'])
+def create_task():
+    if not 'username' in request.json or not 'password' in request.json or not 'token' in request.json:
+      return "Invalid request"
+    return request.json['username']
 
 if __name__ == "__main__":
   app.run(debug=True)
